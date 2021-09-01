418edddb7e19ae9675fb8dd23b64bae2d55f2780
Pitzaza <djzaza123@gmail.com>
2021-09-01 15:24:39 +0700

    edit

--- src/CalculateGrade.java
+++ src/CalculateGrade.java
@@ -1,20 +1,31 @@
-
 public class CalculateGrade {
-	private int score;
-
-	public CalculateGrade(int score) {
-	
-	}
-
-	public int getScore() {
-		return score;
-	}
-
-	public void setScore(int score) {
-		this.score = score;
-	}
-	
-	public char calcuate() {
-        return 'A';
+    private int score;
+    public CalculateGrade(int score) {
+        this.score = score;
     }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+    public char calcuate() {
+        char grade;
+        
+        if(score >= 80) {
+            grade = 'A';
+        }else if(score >= 70) {
+            grade = 'B';
+        }else if(score >= 60) {
+            grade = 'C';
+        }else if(score >= 50) {
+            grade = 'D';
+        }else {
+            grade = 'F';
+        }
+        
+        return grade;
+    }
+    
 }
