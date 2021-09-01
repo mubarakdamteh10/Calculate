216f489bd9b5445ff880ff1743269459f01978a9
iwnlyfe <iwnlyfe@hotmail.com>
2021-09-01 15:09:13 +0700

    add simple function calculateGrade

--- src/CalculateGrade.java
+++ src/CalculateGrade.java
@@ -1,5 +1,20 @@
 
 public class CalculateGrade {
+	private int score;
 
+	public CalculateGrade(int score) {
+	
+	}
 
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public char calcuate() {
+        return 'A';
+    }
 }
